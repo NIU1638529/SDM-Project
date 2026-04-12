414c80f21efa8ed993a5553043400ee8bb9682a4
NIU1638529 <adalsolis10@gmail.com>
2026-04-12 06:09:04 +0200

    Ajuste de configuracion

--- A/A.2/UploadCSV(actualizado).py
+++ A/A.2/UploadCSV(actualizado).py
@@ -6,7 +6,7 @@ from neo4j import GraphDatabase
 # NEO4J configuration ()
 NEO4J_URI = "neo4j://127.0.0.1:7687"
 NEO4J_USER = "neo4j"
-NEO4J_PASSWORD = "adal2003"#"sdmproject"
+NEO4J_PASSWORD = "sdmproject"
 
 # Directory where your CSV files are located
 CSV_DIR = Path(r"./nodes_and_relations") 
